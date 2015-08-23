4fa86e3dd50ec952aa9102fa00001358f67b2160
魏百平 <bpwei@v5.cn>
2015-08-23 23:01:11 +0800

    删除无用代码

--- app/src/main/java/com/example/wbp/practice/activity/CircularImageViewActivity.java
+++ app/src/main/java/com/example/wbp/practice/activity/CircularImageViewActivity.java
@@ -35,33 +35,4 @@ import java.io.InputStream;
 public class CircularImageViewActivity extends Activity {
 	@ViewById(R.id.circular_image_view)
 	CircularImageView circularImageView;
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-
-
-	}
-
-	@AfterInject
-	void afterViews(){
-
-
-	}
-	@Click(R.id.circular_image_view)
-	void click(){
-		Toast.makeText(this,"dianji",Toast.LENGTH_SHORT).show();
-		Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.sss).copy(Bitmap.Config.ARGB_8888, true);
-		Paint p = new Paint();
-		p.setAntiAlias(true); //去锯齿
-		p.setColor(Color.BLACK);
-		p.setStyle(Paint.Style.STROKE);
-		Canvas canvas = new Canvas(bitmap);  //bitmap就是我们原来的图,比如头像
-		p.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));  //因为我们先画了图所以DST_IN
-		int radius = bitmap.getWidth()/4; //假设图片是正方形的
-		canvas.drawCircle(radius, radius, radius, p);
-		circularImageView.draw(canvas);
-		circularImageView.invalidate();
-	}
-
 }
