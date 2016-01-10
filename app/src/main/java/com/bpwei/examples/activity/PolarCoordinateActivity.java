845d5ecfb211e9ffd91fddab30d323f04b46b52f
魏百平 <bpwei@v5.cn>
2016-01-10 21:36:54 +0800

    1.增加蓝牙收发功能 2.增加查看已选点功能 3.修改部分界面

--- app/src/main/java/com/bpwei/examples/activity/PolarCoordinateActivity.java
+++ app/src/main/java/com/bpwei/examples/activity/PolarCoordinateActivity.java
@@ -1,10 +1,15 @@
 package com.bpwei.examples.activity;
 
+import android.view.View;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
 
 import com.bpwei.examples.MainApp;
 import com.bpwei.examples.MainApp_;
 import com.bpwei.examples.R;
+import com.bpwei.examples.bean.UniversalAdapter;
+import com.bpwei.examples.bean.UniversalItem;
+import com.bpwei.examples.bean.ViewHolder;
 import com.bpwei.examples.entites.PolarPoint;
 import com.bpwei.examples.sp.ScreenSP_;
 import com.bpwei.examples.views.PolarCoordinateView;
@@ -16,6 +21,7 @@ import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 import org.androidannotations.annotations.sharedpreferences.Pref;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -26,30 +32,69 @@ public class PolarCoordinateActivity extends BaseActivity {
 
 	@ViewById(R.id.v_polar_coordinate)
 	PolarCoordinateView polarCoordinateView;
+	@ViewById(R.id.lv_coordinates)
+	ListView lvCoordinates;
 
 	@App
 	MainApp app;
-
 	@Pref
 	ScreenSP_ screenSP;
 
+	UniversalAdapter<UniversalItem> adapter;
+	List<UniversalItem> listDatas = new ArrayList<>();
+
 	@AfterViews
-	void afterViews(){
-		RelativeLayout.LayoutParams rlParams = (RelativeLayout.LayoutParams)polarCoordinateView.getLayoutParams();
+	void afterViews() {
+		RelativeLayout.LayoutParams rlParams = (RelativeLayout.LayoutParams) polarCoordinateView.getLayoutParams();
 		rlParams.height = screenSP.width().get();
 
 		polarCoordinateView.setLayoutParams(rlParams);
 	}
 
 	@Click(R.id.tv_clear)
-	void clearClick(){
+	void clearClick() {
 		polarCoordinateView.clearPolarMap();
 		app.toast(R.string.clear);
 	}
 
+	@Click(R.id.tv_check)
+	void checkClick() {
+		listDatas.clear();
+		List<PolarPoint> polarPoints = polarCoordinateView.getSelecetedPoints();
+		for (int i = 0; i < polarPoints.size(); i++) {
+			UniversalItem item = new UniversalItem();
+			item.setTitle(i + ": ");
+			item.setDesc("(" + polarPoints.get(i).getRadius() + ", " + polarPoints.get(i).getAngle() + ")");
+			listDatas.add(item);
+		}
+
+		adapter = new UniversalAdapter<UniversalItem>(this,
+				R.layout.item_coordinate, listDatas) {
+			@Override
+			public void bindItem(ViewHolder holder, UniversalItem item) {
+				holder.setText(R.id.tv_title, item.getTitle());
+				holder.setText(R.id.tv_describe, item.getDesc());
+			}
+		};
+
+		lvCoordinates.setAdapter(adapter);
+
+		showHideCoordinateList(true);
+	}
+
+	@Click(R.id.title_back)
+	void backClick() {
+		showHideCoordinateList(false);
+	}
+
 	@Click(R.id.tv_send)
-	void sendClick(){
+	void sendClick() {
 		List<PolarPoint> polarPoints = polarCoordinateView.getSelecetedPoints();
 		app.toast(polarPoints.toString());
+	}
+
+	private void showHideCoordinateList(boolean show) {
+		polarCoordinateView.setVisibility(show ? View.GONE : View.VISIBLE);
+		lvCoordinates.setVisibility(show ? View.VISIBLE : View.GONE);
 	}
 }
